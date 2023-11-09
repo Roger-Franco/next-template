2f8b20f2d8b4f00472e5c6a8d677a9a613ce6f4e
Roger Franco <rogerfrancogalt@gmail.com>
2023-11-08 21:16:51 -0400

    Inicio do projeto e configuração do typescript e tailwind

---
+++ info.md
@@ -0,0 +1,10 @@
+npx create-next-app admin-template
+
+tsconfig.json
+
+nom run dev (startar o sistema e instala o typescipt se criamos o arquivo da linha 3)
+
+
+npm install -D tailwindcss postcss autoprefixer
+
+npx tailwindcss init -p
---
+++ src/pages/index.tsx
@@ -0,0 +1,12 @@
+
+export default function Home() {
+  return (
+    <div className={`
+    h-screen
+    flex justify-center items-center 
+    bg-gradient-to-r from-green-500 to-blue-500
+    `}>
+      <h1 className="text-3xl font-bold underline">Admin</h1>
+    </div>
+  )
+}
