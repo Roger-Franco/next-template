ad8d1d85f5653fd91ff1af3acc77ae981ba68174
Roger Franco <rogerfrancogalt@gmail.com>
2023-11-09 09:51:39 -0400

    Estrutura Inicial

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,9 @@
+import Layout from '../components/template/Layout';
 
 export default function Home() {
   return (
-    <div className={`
-    h-screen
-    flex justify-center items-center 
-    bg-gradient-to-r from-green-500 to-blue-500
-    `}>
-      <h1 className="text-3xl font-bold underline">Admin</h1>
-    </div>
+    <Layout titulo='Página Inicial' subtitulo='Estamos construindo um template Admin!'>
+      <h3>Conteúdo</h3>
+    </Layout>
   )
 }
