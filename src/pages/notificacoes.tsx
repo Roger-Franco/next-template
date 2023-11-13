03951b303d172fbd8b8573bbc4dd994616efa3d0
Roger Franco <rogerfrancogalt@gmail.com>
2023-11-13 11:51:56 -0400

    Context API #02

--- src/pages/notificacoes.tsx
+++ src/pages/notificacoes.tsx
@@ -1,9 +1,16 @@
 import Layout from '../components/template/Layout';
+// import { AppConsumer } from '../data/context/AppContext';
+import useAppData from '../data/hook/useAppData';
 
 export default function Notificações() {
+
+  const dados = useAppData()
   return (
     <Layout titulo='Notificações' subtitulo='Aqui você irá gerencia as suas notificações!'>
-      <h3>Conteúdo</h3>
+      {/* <AppConsumer>
+        {dados => <h3>{dados.nome}</h3>}
+      </AppConsumer> */}
+      <h3>{dados.nome}</h3>
     </Layout>
   )
 }
