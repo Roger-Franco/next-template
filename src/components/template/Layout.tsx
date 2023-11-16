0f75beb413c67ca8fd7831081afff0f49c2be266
Roger Franco <rogerfrancogalt@gmail.com>
2023-11-16 12:56:58 -0400

    Protegendo ROtas #3 - Com function ao invés de componente

--- src/components/template/Layout.tsx
+++ src/components/template/Layout.tsx
@@ -3,7 +3,8 @@ import MenuLateral from './MenuLateral'
 import Cabecalho from './Cabecalho'
 import Conteudo from './Conteudo'
 import useAppData from '../../data/hook/useAppData'
-import ForcarAutenticacao from '../auth/ForcarAutenticacao'
+import forcarAutenticacao from '../../functions/ForcarAutenticacao'
+// import ForcarAutenticacao from '../auth/ForcarAutenticacao'
 
 interface LayoutProps {
   titulo: string
@@ -13,9 +14,9 @@ interface LayoutProps {
 
 export default function Layout(props: LayoutProps) {
   const {tema} = useAppData()
-
-  return (
-    <ForcarAutenticacao>
+  
+  return forcarAutenticacao(
+    // <ForcarAutenticacao>
       <div className={`${tema} flex h-screen w-screen`}>
         <MenuLateral />
         <div className={`dark:bg-gray-800 flex flex-col w-full p-7 bg-gray-300`}>
@@ -25,6 +26,6 @@ export default function Layout(props: LayoutProps) {
           </Conteudo>
         </div>
       </div>
-    </ForcarAutenticacao>
+    // </ForcarAutenticacao>
   )
 }
